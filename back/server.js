5dfb6a7b4a25a414a24b9d36a470dad8234ee7ef
sandhya-paume <sandhya.paume@gmail.com>
2023-02-03 11:46:08 +0100

    Création du repo P5

--- back/server.js
+++ back/server.js
@@ -33,6 +33,7 @@ const errorHandler = error => {
   }
 };
 
+
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
@@ -40,6 +41,9 @@ server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
+
 });
 
 server.listen(port);
+
+
